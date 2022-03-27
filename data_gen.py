2d9dbbcacc306bc9fc740c48ab54e6ad21231a05
LOUIS CAMMARATA <louis_cammarata@g.harvard.edu>
2022-03-27 16:13:35 -0400

    updates and exp4

--- data_gen.py
+++ data_gen.py
@@ -53,7 +53,8 @@ def sample_alt_adj_matrix_sym(n, K, a, b, exact=True):
         coms = np.array([np.repeat(i,int(n/K)) for i in range(K)]).flatten()
         pi_mat = np.array([np.eye(1,K,i)[0] for i in coms])
     else:
-        pi_mat = np.random.multinomial(1, [1/K]*K, size=n)
+        # pi_mat = np.random.multinomial(1, [1/K]*K, size=n)
+        pi_mat = np.random.multinomial(1, [0.4,0.6], size=n)
     # Construct Omega
     Omega = multi_dot([pi_mat, P, transpose(pi_mat)])
     # Sample adjacency matrix
@@ -66,7 +67,7 @@ def sample_alt_adj_matrix_sym(n, K, a, b, exact=True):
 def sample_alt_adj_matrix_rank1(n, K, eta, c, exact=True):
     '''
     Function used to sample a symmetric adjacency matrix under the alternative
-    hypothesis with K communities. The rank-1 matrix P is defined as eta*eta^T. 
+    hypothesis with K communities. The rank-1 matrix P is defined as c*eta*eta^T. 
     Membership vectors are pure (n/K nodes per class) or sampled i.i.d. from a 
     Multinomial(1/K,...,1/K). This allows to create the ideal matrix Omega, which is 
     used to sample A with A[i,j]~Bernoulli(Omega[i,j]) for all off-diagonal 
@@ -138,11 +139,47 @@ def sample_alt_adj_matrix_sym2(n, K, a, b, c, exact):
     return(adj)
 
 
-def sample_alt_adj_matrix_asym(n, K, a, b):
+def sample_MMSBM_alt_adj_matrix_sym(n, K, a, b):
+    '''
+    Function used to sample a symmetric adjacency matrix under the alternative
+    hypothesis with K communities. The matrix P is defined with all off-diagonal
+    entries equal to b and diagonal entries equal to a. 
+    Membership vectors are sampled from a Dirichlet distribution with equal 
+    probability for each class. This allows to create the ideal matrix Omega, which 
+    is used to sample A with A[i,j]~Bernoulli(Omega[i,j]) for all off-diagonal 
+    elements, and A[i,i]=0 for all diagonal elements (no self-edge).
+    
+    Args:
+      n: (int) number of nodes
+      K: (int) number of communities
+      a: (float) edge probability within a community
+      b: (float) edge probability between distinct communities
+      
+    Returns:
+      An nxn adjacency matrix  
+    '''
+    # Construct P
+    P = (a-b)*np.identity(K)+b*np.ones(K)
+    # Sample membership vectors
+    pi_mat = np.random.dirichlet(alpha = 0.1*np.ones(K), size = n)
+    # Construct Omega
+    Omega = multi_dot([pi_mat, P, transpose(pi_mat)])
+    # Sample adjacency matrix
+    bern_vars = np.random.binomial(n=1, p=Omega)
+    tri = np.triu(bern_vars, k=1)
+    adj = tri+transpose(tri)
+    return(adj)
+
+
+def sample_MMSBM_alt_adj_matrix_asym(n, K, a, b):
     '''
     Function used to sample an asymmetric adjacency matrix under the alternative
     hypothesis with K communities. The matrix P is defined with all off-diagonal
-    entries sampled from Unif[b-e,b+e] where e<<b, and diagonal entries equal to a.     Membership vectors are sampled from a Dirichlet distribution with equal             probability for each class. This allows to create the ideal matrix Omega, which     is used to sample A with A[i,j]~Bernoulli(Omega[i,j]) for all off-diagonal           elements, and A[i,i]=0 for all diagonal elements (no self-edge).
+    entries sampled from Unif[b-e,b+e] where e<<b, and diagonal entries equal to a. 
+    Membership vectors are sampled from a Dirichlet distribution with a given
+    probability for each class. This allows to create the ideal matrix Omega, which 
+    is used to sample A with A[i,j]~Bernoulli(Omega[i,j]) for all off-diagonal 
+    elements, and A[i,i]=0 for all diagonal elements (no self-edge).
     
     Args:
       n: (int) number of nodes
@@ -160,7 +197,40 @@ def sample_alt_adj_matrix_asym(n, K, a, b):
     tri[np.triu_indices(K,1)] = unif_vars
     P = tri+transpose(tri)+a*np.identity(K)
     # Sample membership vectors
-    pi_mat = np.random.dirichlet(alpha = np.ones(K), size = n)
+    probs = np.random.uniform(0,10,K)
+    pi_mat = np.random.dirichlet(alpha = probs, size = n)
+    # Construct Omega
+    Omega = multi_dot([pi_mat, P, transpose(pi_mat)])
+    # Sample adjacency matrix
+    bern_vars = np.random.binomial(n=1, p=Omega)
+    tri = np.triu(bern_vars, k=1)
+    adj = tri+transpose(tri)
+    return(adj)
+
+
+def sample_MMSBM_alt_adj_matrix_asym2(n, K, eta, c, probs):
+    '''
+    Function used to sample an asymmetric adjacency matrix under the alternative
+    hypothesis with K communities. The rank-1 matrix P is defined as c*eta*eta^T. 
+    Membership vectors are sampled from a Dirichlet distribution with a given
+    probability for each class. This allows to create the ideal matrix Omega, which 
+    is used to sample A with A[i,j]~Bernoulli(Omega[i,j]) for all off-diagonal 
+    elements, and A[i,i]=0 for all diagonal elements (no self-edge).
+    
+    Args:
+      n: (int) number of nodes
+      K: (int) number of communities
+      eta: (Numpy array) unique eigenvector of P
+      c: (float) nonzero eigenvalue of P
+      probs: (Numpy array) Dirichlet parameters for the mixed membership vectors
+      
+    Returns:
+      An nxn adjacency matrix  
+    '''
+    # Construct P
+    P = c*dot(eta,transpose(eta))
+    # Sample membership vectors
+    pi_mat = np.random.dirichlet(alpha = probs, size = n)
     # Construct Omega
     Omega = multi_dot([pi_mat, P, transpose(pi_mat)])
     # Sample adjacency matrix
--- stat_test.py
+++ stat_test.py
@@ -3,6 +3,9 @@ from numpy import transpose, trace, multiply, power, dot
 from numpy.linalg import multi_dot, matrix_power
 import scipy.stats as ss
 from scipy.special import comb
+from sklearn.cluster import KMeans
+from TracyWidom import TracyWidom
+import itertools
 
 
 def degree_test(A, level, two_sided=False):
@@ -170,3 +173,440 @@ def PET_test(A, level):
     reject = (pval<=level)
     return({'test_stat': T, 'p_val': pval, 'reject': reject})
 
+
+def lei_test(A, K, level):
+    '''
+    Function used to perform the eigenvalue-based test of Lei (2016). 
+    The test statistic uses the largest singular value of a residual matrix 
+    obtained by subtracting the estimated block mean effect of the adjacency matrix
+    Args:
+      A: (Numpy array) adjacency matrix
+      K: (int) number of communities in the null SBM model
+      level: (float) level of the test
+      
+    Returns:
+      A dictionary containing the test statistic, asymptotic p-value and Boolean
+      indicating whether the null hypothesis (only one community) was rejected.
+    '''
+    # Helpers
+    n = A.shape[0]
+    
+    # Estimate community membership with spectral clustering
+    w, v = np.linalg.eigh(A)
+    magnitude_ordering = np.argsort(-np.abs(w))
+    w = w[magnitude_ordering]
+    v = v.T[magnitude_ordering].T
+    v_selected = v[:,:K]
+    kmeans = KMeans(n_clusters=K, random_state=0).fit(v_selected)
+    z_hat = kmeans.labels_
+    pi_mat_hat = np.array([np.eye(1,K,c)[0] for c in z_hat])
+    
+    # Compute residual matrix A_tilde
+    N_hat = {k: np.where(z_hat==k)[0] for k in range(K)}
+    n_hat = {k: np.sum(z_hat==k) for k in range(K)}
+    P_hat = np.zeros((K,K))
+    for k,l in itertools.combinations_with_replacement(range(K),2):
+        if (k!=l):
+            P_hat[k,l] = np.sum(A[np.ix_(N_hat[k],N_hat[l])])/(n_hat[k]*n_hat[l])
+            P_hat[l,k] = P_hat[k,l]
+        else:
+            P_hat[k,k] = np.sum(A[np.ix_(N_hat[k],N_hat[k])])/(n_hat[k]*(n_hat[k]-1))
+    Omega_hat = multi_dot([pi_mat_hat, P_hat, transpose(pi_mat_hat)])
+    A_tilde = (A-Omega_hat)/np.sqrt((n-1)*Omega_hat*(1-Omega_hat))
+    np.fill_diagonal(A_tilde, 0)
+    
+    # Compute test statistic
+    w_tilde, _ = np.linalg.eigh(A_tilde)
+    sig1 = np.max(np.abs(w_tilde))
+    T = (n**(2/3))*(sig1-2)
+    tw1 = TracyWidom(beta=1)
+    pval = 1-tw1.cdf(T)
+    reject = (pval<level/2)
+
+    return({'test_stat': T, 'p_val': pval, 'reject': reject})
+
+
+def wangbickel_likelihood_method(A, Kmax, tol=1e-8, tol_fp=1e-4, max_iter=100, init='random', seed=13):
+    '''
+    Function to compute the model selection criterion of Wang & Bickel (2016)
+    Args:
+      A: (Numpy array) adjacency matrix
+      Kmax: (int) maximum value of K in Wang & Bickel (2016)
+      tol: (float) tolerance level to stop the EM algorithm
+      tol_fp: (float) tolerance level to stop the fixed point iterations
+      max_iter: (int) maximum number of iterations
+      init: (str) initialization strategy for tau, one of 'spectral' or 'random'
+      seed: (int) seed for initialization of the EM algorithm
+      
+    Returns:
+      Value of the model selection criterion
+    '''
+    # Optimize variational log-likelihood
+    n = A.shape[0]
+    J = np.array([optimize_varloglik(A, k+1, tol=tol, tol_fp=tol_fp, max_iter=max_iter, init=init, seed=13)
+                  for k in range(Kmax)])
+    lambda_list = np.arange(0, 0.3, 1e-3)
+    entropy = -np.inf
+    for lambd in lambda_list:
+        betas = np.array([beta(k+1, J[k], n, lambd, seed=13) for k in range(Kmax)])
+        w = betas/np.sum(betas)
+        entropy_next = -np.sum(w*np.log(w))
+        if (entropy_next > entropy):
+            lambd_star = lambd
+            beta_star = betas
+            entropy = entropy_next
+    betas_dict = {f'beta_{k+1}': beta_star[k] for k in range(Kmax)}
+    K_best = np.argmax(beta_star)+1
+    reject = (K_best==2) # (K_best>1)
+    return({'betas': betas_dict, 'reject': reject})
+
+
+def varloglik(tau, P, a, A):
+    '''
+    Function to compute the variational log-likelihood of the data
+    Args:
+      tau: (Numpy array) variational parameters (parameter of a multinomial for the K clusters)
+      P: (Numpy array) community mixing matrix
+      a: (Numpy array) multinomial parameters of the SBM cluster assignment
+      A: (Numpy array) adjacency matrix
+    Returns:
+      The value of the variational log-likelihood
+    '''
+    n = A.shape[0]
+    M = np.ones((n,n))
+    np.fill_diagonal(M,0)
+    J = np.sum(np.log(a)*np.sum(tau, axis=0))
+    J += -np.sum(tau*np.log(tau))
+    J += 0.5*np.sum(multi_dot([tau, np.log(P), tau.T])*A*M)
+    J += 0.5*np.sum(multi_dot([tau, np.log(1-P), tau.T])*(1-A)*M)
+    return J
+
+
+def ff(x, a, P, A):
+    '''
+    Function used to solve for tau (parameter of a multinomial for the K clusters)
+    using a fixed point iteratio algorithm
+    Args:
+      x: (Numpy array) input, corresponds to the current estimate of tau
+      a: (Numpy array) multinomial parameters of the SBM cluster assignment
+      P: (Numpy array) community mixing matrix
+      A: (Numpy array) adjacency matrix
+      
+    Returns:
+      The updated value of tau
+    '''
+    n = A.shape[0]
+    M = np.ones((n,n))
+    np.fill_diagonal(M,0)
+    log_y = multi_dot([A, x, np.log(P)])
+    log_y += multi_dot([M*(1-A), x, np.log(1-P)])
+    log_y += np.log(a)
+    log_y += -np.mean(log_y)
+    y = np.exp(log_y)
+    y = y/np.sum(y, axis=1)[:,np.newaxis]
+    return y
+
+
+def fixed_point_method(ff, x, a, P, A, tol=1e-4):
+    '''
+    Function to run fixed point iteration algorithm to the function ff, i.e.
+    find the fixed point x* such that x* = ff(x*)
+    Args:
+      ff: (function) function for fixed point interation
+      x: (Numpy array) input, corresponds to the current estimate of tau
+      a: (Numpy array) multinomial parameters of the SBM cluster assignment
+      P: (Numpy array) community mixing matrix
+      A: (Numpy array) adjacency matrix
+      tol: (float) tolerance level to stop the algorithm
+      
+    Returns:
+      The fixed point of ff
+    '''
+    c = 1
+    while (np.sum(np.abs(x-ff(x, a, P, A))) > tol):
+        x = ff(x, a, P, A)
+        c += 1
+        if (c>5000):
+            # print('tau failed to converge')
+            break
+    return x
+
+
+def optimize_varloglik(A, KK, tol=1e-8, tol_fp=1e-4, max_iter=100, init='random', seed=13):
+    '''
+    Function to optimize the variational log-likelihood using variational EM 
+    (from Daudin, Picard and Robin, 2006)
+    Args:
+      A: (Numpy array) adjacency matrix
+      KK: (int) the desired number of communities
+      tol: (float) tolerance level to stop the EM algorithm
+      tol_fp: (float) tolerance level to stop the fixed point iterations
+      max_iter: (int) maximum number of iterations
+      init: (str) initialization strategy for tau, one of 'spectral' or 'random'
+      seed: (int) seed for initialization of the EM algorithm
+      
+    Returns:
+      The maximized variational log-likelihood
+    '''
+    
+    n = A.shape[0]
+    
+    if (KK>1):
+        # For tau_init use spectral clustering on the Laplacian
+        eps = 1e-8
+        
+        if (init=='spectral'):
+            D_invsqrt = np.diag((np.sum(A+eps, axis=1)**(-1/2)))
+            L = multi_dot([D_invsqrt,A+eps,D_invsqrt])
+            w, v = np.linalg.eigh(L)
+            magnitude_ordering = np.argsort(-np.abs(w))
+            w = w[magnitude_ordering]
+            v = v.T[magnitude_ordering].T
+            v_selected = v[:,:KK]
+            kmeans = KMeans(n_clusters=KK, random_state=0).fit(v_selected)
+            z_init = kmeans.labels_
+            tau_init = np.array([np.eye(1,KK,c)[0] for c in z_init])
+#             tau_init_clipped = np.clip(tau_init, a_min=0.1, a_max=None)
+#             tau_init = tau_init_clipped/np.sum(tau_init_clipped)
+        # Or initialize based on random
+        if (init=='random'):
+            np.random.seed(seed)
+            tau_init = np.random.uniform(0,1,(n,KK))
+            tau_init = np.divide(tau_init, np.sum(tau_init, axis=1)[:, np.newaxis])
+        
+
+        # EM algorithm parameters
+        n = A.shape[0]
+        M = np.ones((n,n))
+        np.fill_diagonal(M,0)
+        clip_val = 1e-8
+
+        # Start with initial guess for tau, a and P
+        a_new = np.mean(tau_init, axis=0)
+        a_new_clipped = np.clip(a_new, a_min=clip_val, a_max=None)
+        a_new = a_new_clipped/np.sum(a_new_clipped)
+        P_new = multi_dot([tau_init.T, A, tau_init])/multi_dot([tau_init.T, M, tau_init])
+        P_new = np.clip(P_new, a_min=clip_val, a_max=1-clip_val)
+        tau_new = fixed_point_method(ff, tau_init, a_new, P_new, A, tol=tol_fp)
+
+        delta_theta = np.inf
+        c = 1
+        while (delta_theta>tol):
+
+            # Store previous values
+            a_old = a_new
+            P_old = P_new
+            tau_old = tau_new
+
+            # Update a_new, P_new, tau_new
+            a_new = np.mean(tau_old, axis=0)
+            a_new_clipped = np.clip(a_new, a_min=clip_val, a_max=None)
+            a_new = a_new_clipped/np.sum(a_new_clipped)
+            P_new = multi_dot([tau_old.T, A, tau_old])/multi_dot([tau_old.T, M, tau_old])
+            P_new = np.clip(P_new, a_min=clip_val, a_max=1-clip_val)
+            tau_new = fixed_point_method(ff, tau_old, a_new, P_new, A, tol=tol_fp)
+
+            # Assess convergence
+            c += 1
+            delta_theta = np.sum(np.abs(P_new-P_old))+np.sum(np.abs(a_new-a_old))
+            if (c>max_iter):
+                break
+
+        tau_new_clipped = np.clip(tau_new, a_min=clip_val, a_max=None)
+        tau_new = np.divide(tau_new_clipped, np.sum(tau_new_clipped, axis=1)[:,np.newaxis])
+    
+    else:
+        tau_new = np.ones((n,1))
+        a_new = np.array([1])
+        P_new = np.array([[np.sum(A)/(n*(n-1))]])
+    
+    J = varloglik(tau_new, P_new, a_new, A)
+    
+    return J
+
+
+def beta(KK, J, n, lambd, seed=13):
+    '''
+    Function to compute the model selection criterion of Wang & Bickel (2016)
+    Args:
+      A: (Numpy array) adjacency matrix
+      seed: (int) seed for initialization of the EM algorithm
+      
+    Returns:
+      Value of the model selection criterion
+    '''
+    
+    b = J-lambd*KK*(1+KK)*n*np.log(n)/2
+    return b
+
+
+
+
+
+
+# def wangbickel_likelihood_method(A, Kmax, tol=1e-8, tol_fp=1e-10, max_iter=6000, seed=13):
+#     '''
+#     Function to compute the model selection criterion of Wang & Bickel (2016)
+#     Args:
+#       A: (Numpy array) adjacency matrix
+#       Kmax: (int) maximum value of K in Wang & Bickel (2016)
+#       tol: (float) tolerance level to stop the EM algorithm
+#       tol_fp: (float) tolerance level to stop the fixed point iterations
+#       max_iter: (int) maximum number of iterations
+#       seed: (int) seed for initialization of the EM algorithm
+      
+#     Returns:
+#       Value of the model selection criterion
+#     '''
+#     # Optimize variational log-likelihood
+#     n = A.shape[0]
+#     J = np.array([optimize_varloglik(A, k+1, tol=tol, tol_fp=tol_fp, max_iter=max_iter, seed=seed) for k in range(Kmax)]) 
+#     lambda_list = np.array([0]) # np.arange(0, 0.3, 1e-3)
+#     entropy = -np.inf
+#     for lambd in lambda_list:
+#         betas = np.array([beta(k+1, J[k], n, lambd, seed=13) for k in range(Kmax)])
+#         w = betas/np.sum(betas)
+#         entropy_next = -np.sum(w*np.log(w))
+#         if (entropy_next > entropy):
+#             lambd_star = lambd
+#             beta_star = betas
+#             entropy = entropy_next
+#     betas_dict = {f'beta_{k+1}': beta_star[k] for k in range(Kmax)}
+#     K_best = np.argmax(beta_star)+1
+#     reject = (K_best==2) # (K_best>1)
+#     return({'betas': betas_dict, 'reject': reject})
+        
+
+# def varloglik(tau, P, a, A):
+#     '''
+#     Function to compute the variational log-likelihood of the data
+#     Args:
+#       tau: (Numpy array) variational parameters (parameter of a multinomial for the K clusters)
+#       P: (Numpy array) community mixing matrix
+#       a: (Numpy array) multinomial parameters of the SBM cluster assignment
+#       A: (Numpy array) adjacency matrix
+#     Returns:
+#       The value of the variational log-likelihood
+#     '''
+#     n = A.shape[0]
+#     M = np.ones((n,n))
+#     np.fill_diagonal(M,0)
+#     J = np.sum(np.log(a)*np.sum(tau, axis=0))
+#     J += -np.sum(tau*np.log(tau))
+#     J += 0.5*np.sum(multi_dot([tau, np.log(P), tau.T])*A*M)
+#     J += 0.5*np.sum(multi_dot([tau, np.log(1-P), tau.T])*(1-A)*M)
+#     return J
+
+
+# def ff(x, a, P, A):
+#     '''
+#     Function used to solve for tau (parameter of a multinomial for the K clusters)
+#     using a fixed point iteratio algorithm
+#     Args:
+#       x: (Numpy array) input, corresponds to the current estimate of tau
+#       a: (Numpy array) multinomial parameters of the SBM cluster assignment
+#       P: (Numpy array) community mixing matrix
+#       A: (Numpy array) adjacency matrix
+      
+#     Returns:
+#       The updated value of tau
+#     '''
+#     n = A.shape[0]
+#     M = np.ones((n,n))
+#     np.fill_diagonal(M,0)
+#     log_y = multi_dot([A, x, np.log(P)])
+#     log_y += multi_dot([M*(1-A), x, np.log(1-P)])
+#     log_y += np.log(a)
+#     log_y += -np.mean(log_y)
+#     y = np.exp(log_y)
+#     y = y/np.sum(y, axis=1)[:,np.newaxis]
+#     return y
+
+
+# def fixed_point_method(ff, x, a, P, A, tol=1e-10):
+#     '''
+#     Function to run fixed point iteration algorithm to the function ff, i.e.
+#     find the fixed point x* such that x* = ff(x*)
+#     Args:
+#       ff: (function) function for fixed point interation
+#       x: (Numpy array) input, corresponds to the current estimate of tau
+#       a: (Numpy array) multinomial parameters of the SBM cluster assignment
+#       P: (Numpy array) community mixing matrix
+#       A: (Numpy array) adjacency matrix
+#       tol: (float) tolerance level to stop the algorithm
+      
+#     Returns:
+#       The fixed point of ff
+#     '''
+#     while (np.linalg.norm(x-ff(x, a, P, A)) > tol*np.linalg.norm(x)):
+#         x = ff(x, a, P, A)
+#     return x
+
+
+# def optimize_varloglik(A, KK, tol=1e-8, tol_fp=1e-10, max_iter=6000, seed=13):
+#     '''
+#     Function to optimize the variational log-likelihood using variational EM 
+#     (from Daudin, Picard and Robin, 2006)
+#     Args:
+#       A: (Numpy array) adjacency matrix
+#       KK: (int) the desired number of communities
+#       tol: (float) tolerance level to stop the EM algorithm
+#       tol_fp: (float) tolerance level to stop the fixed point iterations
+#       max_iter: (int) maximum number of iterations
+#       seed: (int) seed for initialization of the EM algorithm
+      
+#     Returns:
+#       The maximized variational log-likelihood
+#     '''
+    
+#     # EM algorithm parameters
+#     n = A.shape[0]
+#     M = np.ones((n,n))
+#     np.fill_diagonal(M,0)
+#     flag = True
+    
+#     # Start with initial guess for tau
+#     np.random.seed(seed)
+#     c = 0
+#     tau_hat = np.random.uniform(0,1,(n,KK))
+#     tau_hat = np.divide(tau_hat, np.sum(tau_hat, axis=1)[:, np.newaxis])
+#     J = -np.inf
+    
+#     while flag:
+    
+#         # Update a_hat and P_hat
+#         a_hat = np.mean(tau_hat, axis=0)
+#         P_hat = np.zeros((KK,KK))
+#         for k,l in itertools.combinations_with_replacement(range(KK),2):
+#             P_hat[k,l] = multi_dot([tau_hat[:,k, np.newaxis].T, A , tau_hat[:,l, np.newaxis]])[0][0]
+#             P_hat[k,l] = P_hat[k,l]/multi_dot([tau_hat[:,k, np.newaxis].T, M , tau_hat[:,l, np.newaxis]])[0][0]
+#             if (k!=l):
+#                 P_hat[l,k] = P_hat[k,l]
+
+#         # Update tau_hat
+#         tau_hat = fixed_point_method(ff, tau_hat, a_hat, P_hat, A, tol=tol_fp)
+#         J_new = varloglik(tau_hat, P_hat, a_hat, A)
+#         c += 1
+
+#         # Assess convergence
+#         if (J_new-J>=tol*np.abs(J)) & (c<max_iter):
+#             J = J_new
+#         else:
+#             flag = False
+    
+#     return J
+
+
+# def beta(KK, J, n, lambd, seed=13):
+#     '''
+#     Function to compute the model selection criterion of Wang & Bickel (2016)
+#     Args:
+#       A: (Numpy array) adjacency matrix
+#       seed: (int) seed for initialization of the EM algorithm
+      
+#     Returns:
+#       Value of the model selection criterion
+#     '''
+    
+#     b = J-lambd*KK*(1+KK)*n*np.log(n)/2
+#     return b
